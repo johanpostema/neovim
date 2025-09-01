67964221d22ee74af4ca549fff6edae2880bb527
Johan Postema <johan@uxn.nl>
2025-09-01 11:09:58 +0200

    disable pybasedright config

--- plugins/basedpyright.lua
+++ plugins/basedpyright.lua
@@ -1,15 +1,15 @@
---  alleen de config, installeer met MasonInstall basedpyright eerst.
-return {
-	"neovim/nvim-lspconfig",
-	opts = {
-		servers = {
-			basedpyright = {
-				settings = {
-					basedpyright = {
-						typeCheckingMode = "standard",
-					},
-				},
-			},
-		},
-	},
-}
+--  --  alleen de config, installeer met MasonInstall basedpyright eerst.
+--  return {
+--  	"neovim/nvim-lspconfig",
+--  	opts = {
+--  		servers = {
+--  			basedpyright = {
+--  				settings = {
+--  					basedpyright = {
+--  						typeCheckingMode = "standard",
+--  					},
+--  				},
+--  			},
+--  		},
+--  	},
+--  }
