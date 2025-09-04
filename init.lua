56ada905719add55b49e6b542fd79d4a775b8880
Johan Postema <johan@uxn.nl>
2025-09-04 08:31:11 +0200

    add ansible-ft-detect based on content

--- init.lua
+++ init.lua
@@ -1040,5 +1040,7 @@ require("lazy").setup({
 	},
 })
 
+require("custom.include.ansible-ft-detect")
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
