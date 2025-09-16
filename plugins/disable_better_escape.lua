da00f510f6d205dd4e7f13c9f006373d3f2c164d
Johan Postema <johan@uxn.nl>
2025-09-16 10:23:50 +0200

    disble better-escape ivm jk key bindings

---
+++ plugins/disable_better_escape.lua
@@ -0,0 +1,6 @@
+-- better-escape heeft rare mapping voor jk waardoor typen stuk gaat met woorden waar ij in voorkomt.
+-- zie ook het commando :verbose imap jk 
+return {
+  { "max397574/better-escape.nvim", enabled = false },
+}
+
